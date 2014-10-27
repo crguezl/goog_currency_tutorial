78240705e6e5872a169b28d0fb74d8b6ca8747ee
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-10-27 09:42:05 +0000

    making it work. Cahnged service used

---
+++ README.md
@@ -0,0 +1,40 @@
+# Sinatra and Google Currency API – Part 2
+
+Repository accompanying the guest blog post on rubylearning.com
+
+http://rubylearning.com/blog/2013/09/04/sinatra-and-google-currency-api-part-1/
+
+Commits are specific to steps in the tutorial.
+
+## Author: Girish Sonawane
+
+## Observations
+
+When doing bundle I have got this warning:
+
+IMPORTANT! Some of the defaults have changed in Capybara 2.1. If you're experiencing failures,
+please revert to the old behaviour by setting:
+
+    Capybara.configure do |config|
+      config.match = :one
+      config.exact_options = true
+      config.ignore_hidden_elements = true
+      config.visible_text_only = true
+    end
+
+If you're migrating from Capybara 1.x, try:
+
+    Capybara.configure do |config|
+      config.match = :prefer_exact
+      config.ignore_hidden_elements = false
+    end
+
+Details here: http://www.elabs.se/blog/60-introducing-capybara-2-1
+
+### APIs
+
+* https://rate-exchange.appspot.com/
+
+Changed URL to:
+    "http://rate-exchange.appspot.com/currency?from=#{from.upcase}&to=#{to.upcase}&q=#{args.first}"
+
--- lib/goog_currency.rb
+++ lib/goog_currency.rb
@@ -1,20 +1,30 @@
 require "rest_client"
 require "json"
+require "pp"
 
 module GoogCurrency
   def self.method_missing(meth, *args)
+    puts "Method missinf: #{meth}"
+    pp args
     from, to = meth.to_s.split("_to_")
 
+    puts "from =#{from} to=#{to}"
     super(meth, *args) and return if from.nil? or from == "" or to.nil? or to == ""
 
-    response = RestClient.get("http://www.google.com/ig/calculator?hl=en&q=#{args.first}#{from.upcase}=?#{to.upcase}").body
+    #response = RestClient.get("http://www.google.com/ig/calculator?hl=en&q=#{args.first}#{from.upcase}=?#{to.upcase}").body
+    url = "http://rate-exchange.appspot.com/currency?from=#{from.upcase}&to=#{to.upcase}&q=#{args.first}"
+    response = RestClient.get(url).body
+    pp url
+    puts "response = #{response}"
 
     # response is not valid json
     response.gsub!(/(lhs|rhs|error|icc)/, '"\1"')
     response_hash = JSON.parse(response)
+    pp "response_hash = #{response_hash}"
 
     if response_hash['error'].nil? or response_hash['error'] == ""
-      response_hash['rhs'].to_f
+      #response_hash['rhs'].to_f
+      response_hash['v'].to_f
     else
       raise "An error occurred: #{response_hash['error']}"
     end
